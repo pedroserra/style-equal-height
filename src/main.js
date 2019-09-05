462a8e92081c04d24ad0494704a8bbce7c78e361
Pedro Serra <pedro.rserra@gmail.com>
2019-09-05 20:38:30 +0100

    added polyfill for Object.values for older browsers

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import debounce from 'debounce';
+import 'mdn-polyfills/Object.values';
 
 const groups = {};
 
@@ -7,31 +8,28 @@ let element;
 export const trigger = debounce(function() {
     if (!element) {
         element = document.createElement('style');
-        document
-            .body
-            .appendChild(element);
+        document.body.appendChild(element);
     }
     element.innerHTML = '';
 
     let css = [
-        '/* style-equal-height generated tag */', ...Object
-            .values(groups)
-            .map(group => {
-                let heights = Object
-                        .values(document.querySelectorAll(group.selector))
-                        .map(el => {
-                            return el.offsetHeight;
-                        }),
-                    max = Math
-                        .max
-                        .apply(Math, heights);
+        '/* style-equal-height generated tag */',
+        ...Object.values(groups).map(group => {
+            let heights = Object.values(document.querySelectorAll(group.selector))
+                    .map(el => {
+                        return el.offsetHeight;
+                    })
+                    .filter(i => i),
+                max = Math.max.apply(Math, heights);
 
+            if (max >= 0) {
                 if (group.mediaQuery) {
                     return `@media (${group.mediaQuery}) { ${group.selector} { height: ${max}px; } }`;
                 } else {
                     return `${group.selector} { height: ${max}px; } `;
                 }
-            })
+            }
+        }),
     ];
 
     element.innerHTML = '\n' + css.join('\n') + '\n';
@@ -40,7 +38,7 @@ export const trigger = debounce(function() {
 export function add(selector, mediaQuery) {
     groups[selector + '-' + mediaQuery] = {
         selector,
-        mediaQuery
+        mediaQuery,
     };
     trigger();
 }
