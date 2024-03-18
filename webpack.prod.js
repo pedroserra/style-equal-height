4ece02099c650fefa50ae98ecc3332f91653d474
Pedro Serra <pedro.rserra@gmail.com>
2024-03-18 12:50:19 +0000

    upgrade dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,11 @@
 const common = require('./webpack.common');
-const ClosureCompilerPlugin = require('webpack-closure-compiler');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = Object.assign({}, common, {
     mode: 'production',
     devtool: 'source-map',
     optimization: {
-        minimizer: [new ClosureCompilerPlugin({
-                compiler: {
-                    language_in: 'ECMASCRIPT6',
-                    language_out: 'ECMASCRIPT3',
-                    compilation_level: 'SIMPLE',
-                    create_source_map: true
-                },
-                concurrency: 6
-            })]
-    }
+        minimize: true,
+        minimizer: [new TerserPlugin({})],
+    },
 });
